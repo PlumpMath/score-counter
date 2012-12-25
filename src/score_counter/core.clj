b623e7687bd7fd69b22632f2dfc77eb0a111ede2
Erik Svedäng <erik.svedang@gmail.com>
2012-12-25 23:31:10 +0100

    Print text correctly

--- src/score_counter/core.clj
+++ src/score_counter/core.clj
@@ -3,20 +3,23 @@
   (:gen-class))
 
 (def teams [{:name "Erik" :score 100}
-            {:name "Jonas" :score 150}])
+            {:name "Jonas" :score 150}
+            {:name "Klara" :score 200}
+            {:name "Anna" :score 40}])
 
 (defn setup []
   (text-font (create-font "Arial" 20)))
 
 (defn draw-team [nr team]
-  (fill 0)
-  (text-size 100)
-  (text (str (:name team) ": " (:score team))
-        (+ 100 (* 0 nr))
-        (+ 100 (* 100 nr))))
+  (let [slice (/ (height) (count teams))]
+    (fill 0)
+    (text-size (* 0.8 slice))
+    (text (str (:name team) ": " (:score team))
+          (+ 20 (* 0 nr))
+          (+ (* 0.8 slice) (* slice nr)))))
 
 (defn draw []
-  (background 220 220 120)
+  (background 220 (+ 155 (* 100 (sin (* 0.001 (millis))))) 220)
   (fill 0)
   (doall (map (fn [[nr team]] (draw-team nr team))
               (zipmap (range) teams))))
