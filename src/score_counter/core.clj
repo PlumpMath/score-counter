2e23eb4472433f6a18f34c6e792a30f6f16e19d5
Erik Svedäng <erik.svedang@gmail.com>
2012-12-25 12:18:05 +0100

    Initial commit

---
+++ src/score_counter/core.clj
@@ -0,0 +1,7 @@
+(ns score-counter.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/score_counter/core_test.clj
@@ -0,0 +1,7 @@
+(ns score-counter.core-test
+  (:use clojure.test
+        score-counter.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
