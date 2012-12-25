b2332eb768468d51ae1572579a9cf1068d714f35
Erik Svedäng <erik.svedang@gmail.com>
2012-12-26 00:01:33 +0100

    Beginning of key input

--- src/score_counter/core.clj
+++ src/score_counter/core.clj
@@ -1,11 +1,12 @@
 (ns score-counter.core
   (:use quil.core)
+  (:import [java.awt.event KeyEvent])
   (:gen-class))
 
-(def teams [{:name "Erik" :score 100}
-            {:name "Jonas" :score 150}
-            {:name "Klara" :score 200}
-            {:name "Anna" :score 40}])
+(def teams [{:name "Erik" :score 90 :add-key 81}
+            {:name "Jonas" :score 150 :add-key 87}
+            {:name "Klara" :score 200 :add-key 0}
+            {:name "Anna" :score 40 :add-key 0}])
 
 (defn setup []
   (text-font (create-font "Arial" 20)))
@@ -24,11 +25,17 @@
   (doall (map (fn [[nr team]] (draw-team nr team))
               (zipmap (range) teams))))
 
+(defn key-pressed []
+  (let [team (first (filter (fn [{k :add-key}] (= k (key-code))) teams))]
+    (println "Found team " team)))
+
 (defn -main [& args]
        (defsketch score-counter
          :title "Score Counter"
          :setup setup
          :draw draw
+         :key-pressed key-pressed
          :size [800 600]))
 
 (-main)
+
