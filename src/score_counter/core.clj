acac21e184aba48cdb59725bf1bb5bbe80dec04e
Erik Svedäng <erik.svedang@gmail.com>
2012-12-27 16:05:24 +0100

    Saving functionality

--- src/score_counter/core.clj
+++ src/score_counter/core.clj
@@ -3,31 +3,56 @@
   (:import [java.awt.event KeyEvent])
   (:gen-class))
 
-(def teams [{:name "Erik" :score 90 :add-key 81}
-            {:name "Jonas" :score 150 :add-key 87}
-            {:name "Klara" :score 200 :add-key 0}
-            {:name "Anna" :score 40 :add-key 0}])
+(def teams (atom [{:name "Erik" :score 0 :add-key 81 :dec-key 65}]))
 
 (defn setup []
-  (text-font (create-font "Arial" 20)))
+  (text-font (create-font "Arial Black.ttf" 20)))
 
 (defn draw-team [nr team]
-  (let [slice (/ (height) (count teams))]
+  (let [slice (/ (height) (count @teams))]
     (fill 0)
     (text-size (* 0.8 slice))
     (text (str (:name team) ": " (:score team))
-          (+ 20 (* 0 nr))
+          (+ 20)
           (+ (* 0.8 slice) (* slice nr)))))
 
 (defn draw []
-  (background 220 (+ 155 (* 100 (sin (* 0.001 (millis))))) 220)
+  (let [r (+ 200 (* 55 (sin (* 0.0003 (millis)))))
+        g (+ 155 (* 100 (sin (* 0.001 (millis)))))
+        b (+ 155 (* -100 (sin (* 0.0005 (millis)))))]
+    (background r g b))
   (fill 0)
   (doall (map (fn [[nr team]] (draw-team nr team))
-              (zipmap (range) teams))))
+              (zipmap (range) @teams))))
+
+(defn get-index-of-team [team-name]
+  (loop [index 0]
+    (if (= (:name (get @teams index)) team-name)
+      index
+      (if (> index (count @teams))
+        (throw (Exception. (str "Can't find team " team-name ", index is " index)))
+        (recur (inc index))))))
+
+(defn change-score [team-name f]
+  (let [team-index (get-index-of-team team-name)
+        team (nth @teams team-index)
+        new-team (assoc team :score (f (:score team)))
+        new-teams (assoc @teams team-index new-team)]
+    (reset! teams new-teams)))
 
 (defn key-pressed []
-  (let [team (first (filter (fn [{k :add-key}] (= k (key-code))) teams))]
-    (println "Found team " team)))
+  (condp = (key-code)
+    59 (do 
+         (spit "save.txt" (str @teams))
+         (println "Saved save.txt"))
+    76 (do
+         (reset! teams (read-string (slurp "save.txt")))
+         (println "Loaded save.txt"))
+      (do
+        (if-let [team (first (filter (fn [{k :add-key}] (= k (key-code))) @teams))]
+          (change-score (:name team) inc))
+        (if-let [team (first (filter (fn [{k :dec-key}] (= k (key-code))) @teams))]
+          (change-score (:name team) dec)))))
 
 (defn -main [& args]
        (defsketch score-counter
@@ -37,5 +62,5 @@
          :key-pressed key-pressed
          :size [800 600]))
 
-(-main)
+
 
